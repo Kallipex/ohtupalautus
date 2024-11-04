fb8542974536c3312ddd064b08be629d7f99749f
Kalle Hopponen <148634945+Kallipex@users.noreply.github.com>
2024-11-04 23:12:23 +0200

    Added 3 tests to statistics_service_test.py.

--- viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -15,6 +15,31 @@ class PlayerReaderStub:
 class TestStatisticsService(unittest.TestCase):
     def setUp(self):
         # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
-        self.stats = StatisticsService(
-            PlayerReaderStub()
-        )
+        self.stats = StatisticsService(PlayerReaderStub())
+
+    def test_search(self):
+        player = self.stats.search("Semenko")
+        self.assertIsNotNone(player)
+        self.assertEqual(player.name, "Semenko")
+
+        player = self.stats.search("NonExistent")
+        self.assertIsNone(player)
+
+    def test_team(self):
+        edm_players = self.stats.team("EDM")
+        self.assertEqual(len(edm_players), 3)
+        self.assertTrue(all(player.team == "EDM" for player in edm_players))
+
+        pit_players = self.stats.team("PIT")
+        self.assertEqual(len(pit_players), 1)
+        self.assertTrue(all(player.team == "PIT" for player in pit_players))
+
+        no_team_players = self.stats.team("NON")
+        self.assertEqual(len(no_team_players), 0)
+
+    def test_top(self):
+        top_players = self.stats.top(2)
+        self.assertEqual(len(top_players), 3)
+        self.assertEqual(top_players[0].name, "Gretzky")
+        self.assertEqual(top_players[1].name, "Lemieux")
+        self.assertEqual(top_players[2].name, "Yzerman")
