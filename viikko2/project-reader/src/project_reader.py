3309b7b85469afe1fa606024e04594ec73b6db29
Kallipex <148634945+Kallipex@users.noreply.github.com>
2024-11-10 12:00:53 +0200

    Edited project_reader.py. Added funtionality using the toml-module.

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+from toml import loads
 
 class ProjectReader:
     def __init__(self, url):
@@ -8,8 +8,10 @@ class ProjectReader:
 
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
-        content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        content = loads(request.urlopen(self._url).read().decode("utf-8"))["tool"]["poetry"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(content["name"], content["description"], content["dependencies"], content["group"]["dev"]["dependencies"])
+
+if __name__ == "__main__":
+    print(ProjectReader("https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml").get_project())
