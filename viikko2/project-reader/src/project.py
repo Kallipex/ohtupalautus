9413abbe1bfb108e6157b4631dfcae9fef5b75b3
Kallipex <148634945+Kallipex@users.noreply.github.com>
2024-11-10 12:53:54 +0200

    Expanded the functionality of project.py

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,19 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _stringify_content(self, content):
+        return "\n - ".join(content) if len(content) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
-            f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nDescription: {self.description or '-'}\n"
+            f"\nAuthors:\n - {self._stringify_content(self.authors)}\n"
+            f"\nDependencies:\n - {self._stringify_content(self.dependencies)}\n"
+            f"\nDevelopment dependencies:\n - {self._stringify_content(self.dev_dependencies)}"
         )
