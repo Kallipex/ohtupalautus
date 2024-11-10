1ae6c855ea425e1072dc8344e16af790f3cbd384
Kallipex <148634945+Kallipex@users.noreply.github.com>
2024-11-10 12:58:27 +0200

    Updated project_reader.py Adapted the code according to the changes made to project.py

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -11,7 +11,4 @@ class ProjectReader:
         content = loads(request.urlopen(self._url).read().decode("utf-8"))["tool"]["poetry"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(content["name"], content["description"], content["dependencies"], content["group"]["dev"]["dependencies"])
-
-if __name__ == "__main__":
-    print(ProjectReader("https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml").get_project())
+        return Project(content["name"], content["description"], content["authors"], content["dependencies"], content["group"]["dev"]["dependencies"])
