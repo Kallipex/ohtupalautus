4a10a4b32c4fa0149e9ca82636785be6fb9868a0
Kallipex <148634945+Kallipex@users.noreply.github.com>
2024-11-10 23:04:43 +0200

    Updated main program Updated the main program's functionality in accordance to the requirements of exercise 10.

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,19 +5,17 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
         players.append(player)
 
-    print("Oliot:")
+    print(f"Players from FIN:\n")
 
     for player in players:
-        print(player)
+        if player.nationality == 'FIN':
+            print(player)
 
 if __name__ == "__main__":
     main()
