6355db0672448484443392d284f4318d8736a95d
Kallipex <148634945+Kallipex@users.noreply.github.com>
2024-11-11 13:21:37 +0200

    Main program from exercise 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,22 +1,13 @@
-import requests
-from player import Player
+from player_stats import PlayerReader
+from player_stats import PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        if player.nationality == 'FIN':
-            players.append(player)
-
-    players_sorted = sorted(players, key=lambda x: x.points, reverse=True)
-
-    print("Players from FIN:\n")
-
-    for player in players_sorted:
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
